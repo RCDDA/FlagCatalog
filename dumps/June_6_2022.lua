1a3d3cd971fa5d312eff287a2b5d74ba8f8b70f8
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-06-06 12:29:41 -0400

    Create June_6_2022.lua

---
+++ dumps/June_6_2022.lua
@@ -0,0 +1,114 @@
+--[[
+
+  Reformatted :) Took a bit!
+
+  Time of Dump: 12:29 PM, 6/6/2022
+
+]]
+
+local dump_data = {
+    Patterns = {
+        Blank = {
+            PreviewImage = "rbxassetid://9006528498", 
+            Requirement = "CustomColors", 
+            Name = "Blank", 
+            Groups = { "Patterns" }
+        }, 
+        Checkered = {
+            Image = "rbxassetid://9821445756", 
+            Name = "Checkered", 
+            Groups = { "Patterns" }
+        }, 
+        Black_stripes = {
+            Image = "rbxassetid://9834358329", 
+            Name = "Black stripes", 
+            Groups = { "Patterns" }
+        }, 
+        Light_blue_stripes = {
+            Image = "rbxassetid://9834358944", 
+            Name = "Light blue stripes", 
+            Groups = { "Patterns" }
+        }, 
+        Red_stripes = {
+            Image = "rbxassetid://9834358806", 
+            Name = "Red stripes", 
+            Groups = { "Patterns" }
+        },
+        Yellow_stripes = {
+            Image = "rbxassetid://9834358521", 
+            Name = "Yellow stripes", 
+            Groups = { "Patterns" }
+        },
+        Light_green_stripes = {
+            Image = "rbxassetid://9834359058", 
+            Name = "Light green stripes", 
+            Groups = { "Patterns" }
+        },
+        Blue_stripes = {
+            Image = "rbxassetid://9834358146", 
+            Name = "Blue stripes", 
+            Groups = { "Patterns" }
+        },
+        Purple_stripes = {
+            Image = "rbxassetid://9834358677", 
+            Name = "Purple stripes", 
+            Groups = { "Patterns" }
+        }
+    }, Pride = {
+        Progress_Flag = {
+            Image = "rbxassetid://9821163059", 
+            Name = "Progress Flag", 
+            Groups = { "Pride" }
+        },
+        Rainbow = {
+            Image = "rbxassetid://441062128", 
+            Name = "Rainbow", 
+            Groups = { "Pride" }
+        },
+        Transgender = {
+            Image = "rbxassetid://9783874639", 
+            Name = "Transgender", 
+            Groups = { "Pride" }
+        },
+        Pan = {
+            Image = "rbxassetid://9783857390", 
+            Name = "Pan", 
+            Groups = { "Pride" }
+        },
+        Bi = {
+            Image = "rbxassetid://9783873197", 
+            Name = "Bi", 
+            Groups = { "Pride" }
+        },
+        Male = {
+            Image = "rbxassetid://9821171555", 
+            Name = "Male", 
+            Groups = { "Pride" }
+        },
+        Female = {
+            Image = "rbxassetid://9821188867", 
+            Name = "Female", 
+            Groups = { "Pride" }
+        },
+        Ace = {
+            Image = "rbxassetid://9821180004", 
+            Name = "Ace", 
+            Groups = { "Pride" }
+        },
+        Non_Binary = {
+            Image = "rbxassetid://9821185525", 
+            Name = "Non-Binary", 
+            Groups = { "Pride" }
+        },
+        Aro = {
+            Image = "rbxassetid://9821362784", 
+            Name = "Aro", 
+            Groups = { "Pride" }
+        },
+        Omni = {
+            Image = "rbxassetid://9821390210", 
+            Name = "Omni", 
+            Groups = { "Pride" }
+        }
+    }
+}
